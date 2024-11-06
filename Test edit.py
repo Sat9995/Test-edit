03ac993f8aeda101538c562f53f7d21d0b1a1c0b
Sat9995 <saidboy995@gmail.com>
2024-11-06 12:55:23 -0700

    Add files via upload

---
+++ Test edit.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+my_list=list(students)
+sorted_list=sorted(my_list)
+print(sorted_list)
+print(sum(grades[0]),sum(grades[1]),sum(grades[2]),sum(grades[3]),sum(grades[4]))
+print(len(grades[0]),len(grades[1]),len(grades[2]),len(grades[3]),len(grades[4]))
+print(20/5),print(9/4),print(16/4),print(11/3),print(24/5)
+grades_students={'Aaron':(20/5), 'Bilbo':(9/4), 'Johnny':(16/4), 'Khendrik':(11/3), 'Steve':(24/5)}
+print(grades_students)
